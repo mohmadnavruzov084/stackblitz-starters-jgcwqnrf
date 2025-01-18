844702cd152ab4f8b8c6296f7064a58058fa2eb8
mohmadnavruzov084 <git@stackblitz.com>
2025-01-18 11:12:50 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# stackblitz-starters-jgcwqnrf
+
+[Edit in StackBlitz next generation editor ⚡️](https://stackblitz.com/~/github.com/mohmadnavruzov084/stackblitz-starters-jgcwqnrf)
---
+++ script.js
@@ -0,0 +1,27 @@
+'use strict';
+
+let myElement = document.getElementById('myElement');
+myElement.textContent = 'Hello world!';
+myElement.style.backgroundColor = 'lightblue';
+
+let spanTest = document.querySelector('.test');
+spanTest.classList.add('highlighted');
+spanTest.classList.remove('highlighted');
+
+let paragraph = document.createElement('p');
+paragraph.textContent = 'It new paragraph';
+
+let removeParagraph = paragraph.classList.add('pVanRemove');
+removeParagraph.remove();
+
+spanTest.appendChild(paragraph);
+
+let changeUrlImg = document.querySelector('.myLink');
+changeUrlImg.setAttribute("src", "img/cute_cat-2.jpg");
+let changeUrlImg = document.querySelector('.myLink');
+if (changeUrlImg) {
+  console.log('Элемент найден', changeUrlImg)
+   changeUrlImg.setAttribute("src", "img/cute_cat-2.jpg");
+ } else {
+     console.error('Элемент не найден')
+ }
